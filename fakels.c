d858bd0cf4bcbb6dc35c47c0cfe160ed63b5c7b1
tomas-20 <tacuna20@stuy.edu>
2021-11-17 22:37:00 -0500

    made it so that fakels prints out directories and regular files seperately

--- fakels.c
+++ fakels.c
@@ -1,28 +1,34 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
-int get_size(char *filename) {
-  struct stat filestats;
-  stat(filename, &filestats);
-  return filestats.st_size;
+int get_file_size(char *file_name) {
+  struct stat file_stats;
+  stat(file_name, &file_stats);
+  return file_stats.st_size;
 }
 
-void fakels(char *dirname) {
-  DIR *dir = opendir(dirname);
+void show_files(DIR *dir, int type) {
+  rewinddir(dir);
   for (struct dirent *entry; (entry = readdir(dir));) {
-    char *filename = entry->d_name;
-    printf("%s ", filename);
+    int file_type = entry->d_type;
+    if (file_type == type) {
+      char *file_name = entry->d_name;
+      printf("  %s\n", file_name);
+    }
   }
-  closedir(dir);
-  printf("\n");
+}
+
+void fakels(char *dir_name) {
+  DIR *dir = opendir(dir_name);
+  printf("Directories:\n");
+  show_files(dir, DT_DIR);
+  printf("Regular files:\n");
+  show_files(dir, DT_REG);
 }
 
 int main() {
-  //fakels("sandwich");
-  //dirsize("sandwich");
-  printf("%d\n", get_size("wabaloo"));
-  printf("%d\n", get_size("milanesa"));
-  printf("%d\n", get_size("sandwich/milanesa"));
+  fakels("sandwich");
   return 0;
 }
